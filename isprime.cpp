54455f2a6512323810499d2fd5008d46644aa210
rajeev_rajput <rajeevrajput0512@gmail.com>
2020-06-15 22:34:19 +0530

    Added Questions

---
+++ isprime.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n, i;
+  bool isPrime = true;
+
+  cin >> n;
+  for (i = 2; i <= n / 2; ++i)
+  {
+    if (n % i == 0)
+    {
+      isPrime = false;
+      break;
+    }
+  }
+  cout << string( isPrime ? "True" : "False");
+  return 0;
+}
